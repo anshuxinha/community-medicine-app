3569216a2da6968f52f4a10d6a4d8a6dea969be4
anshuxinha <anshuxinha@gmail.com>
2026-07-05 15:37:26 +0530

    refactor: Reset images and mapping to test Gemini Lite image model

--- src/data/updates_images_map.js
+++ src/data/updates_images_map.js
@@ -1,6 +1,2 @@
 export const UPDATES_IMAGES = {
-  "0af865f9-3f6f-4384-a373-22a4faf960dd": require("../../assets/updates/0af865f9-3f6f-4384-a373-22a4faf960dd.png"),
-  "2aa0f12f-e3be-42be-9f8d-980c87deb95e": require("../../assets/updates/2aa0f12f-e3be-42be-9f8d-980c87deb95e.png"),
-  "2c70706f-c322-4f73-ae5f-149a00c4210d": require("../../assets/updates/2c70706f-c322-4f73-ae5f-149a00c4210d.png"),
-  "e7c5e79b-f6c9-433f-815b-a3a470e7e5e3": require("../../assets/updates/e7c5e79b-f6c9-433f-815b-a3a470e7e5e3.png"),
 };
